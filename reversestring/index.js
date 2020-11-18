c8bff2335098c17636dc1b735728c43e96e56b18
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-18 14:41:13 -0500

    polindrome && reversestring

--- reversestring/index.js
+++ reversestring/index.js
@@ -1,1 +1,29 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
 
+function reverse(str) {
+    let reversed = '';
+    for (let character of str){
+        reversed = character + reversed
+    }
+      return reversed;
+  }
+
+module.exports = reverse;
+
+//another solution
+// function reverse(str){
+//     let reversedStr = [...str].reverse().join('')
+//     return reversedStr
+// }
+
+
+
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char+ rev , '')
+//   }
