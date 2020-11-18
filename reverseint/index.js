d489d28bf09053244556d3b203c2bba819ed6447
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-18 14:39:36 -0500

    reverseint solved

---
+++ reverseint/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+
+    let reversed;
+    if (Math.sign(n)===1 ||  n === 0){
+        reversed = parseInt(n.toString().split('').reverse().join(''))
+    } else {
+        reversed = parseInt('-' + parseInt(n.toString().split('').reverse().join('')))    
+    }
+    return reversed
+}
+
+console.log(reverseInt(-5))
+
+//Math.sign() returns 1 is positive number, return 0 if negative number
+//ParseInt 
+//toString
