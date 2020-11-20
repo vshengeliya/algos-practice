5c7a017ffa28c86b3da8cc6a0c14dd8f07f42d2e
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-20 17:12:42 -0500

    chunk solved

---
+++ chunck/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+//create a new array called chunked
+//for each element
+//check if the last element of the chuncked array is legnth of the given size 
+//or if the element deosn't exist
+//push this elemnt in the check chunked as new array
+//else push the element into the ealiser created chunck
+function chunk(array, size) {
+
+    const chunked = [];
+    for (let element of array){
+        const last = chunked[chunked.length-1];
+
+        if (!last || last.length === size){
+            chunked.push([element])
+        } else{
+            last.push(element);
+        }
+    }
+ return chunked;
+
+}
