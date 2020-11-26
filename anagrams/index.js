292a2f09673bbcd04459c37d9229f66e1c7b280e
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-26 12:22:20 -0500

    2nd solution anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -23,29 +23,29 @@ function cleanString(str){
 
 //2nd solution
 
-function anagrams (stringA, stringB){
+// function anagrams (stringA, stringB){
 
-    const aCharMap = buildCharMap(stringA)
-    const bCharMap = buildCharMap(stringB)
+//     const aCharMap = buildCharMap(stringA)
+//     const bCharMap = buildCharMap(stringB)
 
-    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
-        return false
-    }
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+//         return false
+//     }
 
-    for (let char in aCharMap){
-        if (aCharMap[char] !== bCharMap[char]){
-            return false 
-        }
-    }
-    return true
-}
+//     for (let char in aCharMap){
+//         if (aCharMap[char] !== bCharMap[char]){
+//             return false 
+//         }
+//     }
+//     return true
+// }
 
-function buildCharMap(str){
-    const charMap = {}
+// function buildCharMap(str){
+//     const charMap = {}
 
-    for (let char of str.replace(/[^\w]/g, "").toLowerCase()){
-        charMap[char] = charMap[char]+1 || 1;
-    }
+//     for (let char of str.replace(/[^\w]/g, "").toLowerCase()){
+//         charMap[char] = charMap[char]+1 || 1;
+//     }
 
-    return charMap
-}
+//     return charMap
+// }
