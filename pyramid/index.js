41dfff1a4e706bf14b136a262afd4940f607954b
Veronika Dodda <v.shengeliya@gmail.com>
2020-12-07 17:11:22 -0500

    2nd solution added

--- pyramid/index.js
+++ pyramid/index.js
@@ -42,6 +42,28 @@ function pyramid(n) {
         }  
 }
 
-pyramid(3)
+//2nd solution using recursion 
+
+function pyramid(n, row = 0, level = ''){
+
+    if (row === n){
+        return;
+    }
+
+    if (level.length === 2 * n -1 ){
+        console.log(level)
+        return pyramid (n, row +1);
+    }
+    const midpoint = Math.floor((2 * n -1)/2);
+    let add;
+
+    if (midpoint - row <= level.length && midpoint + row >= level.length){
+        add = '#'
+    } else {
+        add =' '
+    }
+    pyramid (n,row, level+add);
+}
+
 
 
